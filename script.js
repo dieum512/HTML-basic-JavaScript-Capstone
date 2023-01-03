b214c63b67206a0446f12e2350ec6aea687402c7
dieum512 <akonkwadieum222@gmail.com>
2023-01-03 12:57:22 +0200

    Add the Rappers section dynamically

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+import data from './datainfo.js';
+
 const click = document.querySelector('.burger');
 const menu = document.querySelector('.nav');
 const header = document.querySelector('.main-header');
+const myCards = data.card;
 
 click.addEventListener('click', () => {
     click.classList.toggle('active');
@@ -13,3 +16,28 @@ document.querySelectorAll('.navlink').forEach((n) => n.addEventListener('click',
     menu.classList.remove('active');
     header.classList.remove('active');
 }));
+
+const cardGrid = document.querySelector('.rappers-cards-container');
+
+function implementCards() {
+    myCards.forEach((card) => {
+        const cardArticle = `
+        <div class="card-div">
+          <div class="card-image">
+            <img src="${card.cardImage}" alt="${card.cardAlt}" />
+          </div>
+          <div class="rapper-story">
+            <h3>${card.cardName}</h3>
+            <h5>${card.cardInfo}</h5>
+            <hr />
+            <p>
+              ${card.cardDetail}
+            </p>
+          </div>
+        </div>
+        `;
+        cardGrid.innerHTML += cardArticle;
+    });
+}
+
+implementCards();
