2b61c5ed0729fe5bb420ab89be6eda95548a1cd2
dieum512 <akonkwadieum222@gmail.com>
2023-01-03 14:46:35 +0200

    Check and Correct linters errors

--- about-page/about.js
+++ about-page/about.js
@@ -3,13 +3,13 @@ const menu = document.querySelector('.nav');
 const header = document.querySelector('.main-header');
 
 click.addEventListener('click', () => {
-    click.classList.toggle('active');
-    menu.classList.toggle('active');
-    header.classList.toggle('active');
+  click.classList.toggle('active');
+  menu.classList.toggle('active');
+  header.classList.toggle('active');
 });
 
 document.querySelectorAll('.navlink').forEach((n) => n.addEventListener('click', () => {
-    click.classList.remove('active');
-    menu.classList.remove('active');
-    header.classList.remove('active');
+  click.classList.remove('active');
+  menu.classList.remove('active');
+  header.classList.remove('active');
 }));
--- script.js
+++ script.js
@@ -6,38 +6,38 @@ const header = document.querySelector('.main-header');
 const myCards = data.card;
 
 click.addEventListener('click', () => {
-    click.classList.toggle('active');
-    menu.classList.toggle('active');
-    header.classList.toggle('active');
+  click.classList.toggle('active');
+  menu.classList.toggle('active');
+  header.classList.toggle('active');
 });
 
 document.querySelectorAll('.navlink').forEach((n) => n.addEventListener('click', () => {
-    click.classList.remove('active');
-    menu.classList.remove('active');
-    header.classList.remove('active');
+  click.classList.remove('active');
+  menu.classList.remove('active');
+  header.classList.remove('active');
 }));
 
 const cardGrid = document.querySelector('.rappers-cards-container');
 
 function implementCards() {
-    myCards.forEach((card) => {
-        const cardArticle = `
-        <div class="card-div">
-          <div class="card-image">
-            <img src="${card.cardImage}" alt="${card.cardAlt}" />
-          </div>
-          <div class="rapper-story">
-            <h3>${card.cardName}</h3>
-            <h5>${card.cardInfo}</h5>
-            <hr />
-            <p>
-              ${card.cardDetail}
-            </p>
-          </div>
-        </div>
-        `;
-        cardGrid.innerHTML += cardArticle;
-    });
+  myCards.forEach((card) => {
+    const cardArticle = `
+    <div class="card-div">
+      <div class="card-image">
+        <img src="${card.cardImage}" alt="${card.cardAlt}" />
+      </div>
+      <div class="rapper-story">
+        <h3>${card.cardName}</h3>
+        <h5>${card.cardInfo}</h5>
+        <hr />
+        <p>
+          ${card.cardDetail}
+        </p>
+      </div>
+    </div>
+    `;
+    cardGrid.innerHTML += cardArticle;
+  });
 }
 
 implementCards();
